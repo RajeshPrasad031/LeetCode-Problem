aca0eae110c079cd40f8b0a4b46e76bc52a7ea0d
Rajesh Prasad <36229584+RajeshPrasad031@users.noreply.github.com>
2022-01-14 20:34:41 +0530

    Update Move_Zeroes.py

--- Move_Zeroes.py
+++ Move_Zeroes.py
@@ -23,3 +23,15 @@ class Solution:
         while(j<n):
             nums[j] = 0
             j+=1
+           
+           
+           '''
+           n = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[n] = nums[n], nums[i]
+                n += 1
+           '''
+           
+           
+           
