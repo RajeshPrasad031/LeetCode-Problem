fbd4465d87f735bb5215c17d482a974061f29889
Rajesh Prasad <36229584+RajeshPrasad031@users.noreply.github.com>
2022-03-24 11:39:54 +0530

    Create Shuffle the Array.py

---
+++ Shuffle the Array.py
@@ -0,0 +1,27 @@
+'''
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+'''
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        first = nums[:n:]
+        second = nums[n::]
+        j,k=0,0
+        for i in range(len(nums)):
+            if(i%2==0):
+                nums[i]=first[j]
+                j+=1
+            else:
+                nums[i]=second[k]
+                k+=1
+        return nums
